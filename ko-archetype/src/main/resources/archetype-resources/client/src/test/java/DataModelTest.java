7287348248b25e2bf59902e4e25ccbfcf92a3638
Eppleton IT <seal@dukescript.com>
2016-12-06 05:18:48 +0100

    IntegrationTest to execute UI testing code in a JavaFX WebView as well pluginless browser

--- ko-archetype/src/main/resources/archetype-resources/client/src/test/java/DataModelTest.java
+++ ko-archetype/src/main/resources/archetype-resources/client/src/test/java/DataModelTest.java
@@ -1,27 +1,22 @@
 package ${package};
 
-import net.java.html.junit.BrowserRunner;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-/** Tests for behavior of your application in real systems. The {@link BrowserRunner}
- * selects all possible presenters from your <code>pom.xml</code> and
- * runs the tests inside of them.
- *
- * See your <code>pom.xml</code> dependency section for details.
+/** Tests for behavior of your application in isolation. Verify
+ * behavior of your MVVC code in a unit test.
  */
-@RunWith(BrowserRunner.class)
+//@RunWith(BrowserRunner.class)
 public class DataModelTest {
     @Test public void testUIModelWithoutUI() {
         Data model = new Data();
 #if ($example.equals("true"))
         model.setMessage("Hello World!");
-        
+
         java.util.List<String> arr = model.getWords();
         assertEquals("Six words always", arr.size(), 6);
         assertEquals("Hello is the first word", "Hello", arr.get(0));
         assertEquals("World is the second word", "World!", arr.get(1));
-#end        
+#end
     }
 }
